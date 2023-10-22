7fe64c8b16a4f2016680e4990aa33968d7f058e8
My Name <mdarfinji45@gmail.com>
2023-10-22 22:13:52 +0800

    added error handling middleware

--- index.js
+++ index.js
@@ -44,6 +44,18 @@ app.use("/api/auth", authroutes)
 app.use("/api/hotels", hotelsroutes)
 // app.use("/api/rooms", authroutes)
 
+// middleware error
+app.use((err,req,res,next) => {
+ 
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+  return res.status(errorStatus).json({
+    success : false,
+    status :errorStatus,
+    message :errorMessage
+  })
+
+})
 
 
 
